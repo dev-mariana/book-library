395b35c55b855f578e7f18d7cc3030e8d3287cea
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-04-26 19:47:58 -0300

    feat: add book struct

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+type Book struct {
+	id string
+	title string
+	description string
+	author string
+	genre string
+	publication_date string
+	created_at string
+	updated_at string
+}
