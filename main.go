e7fe8f02f8730cf5db2166f6634de478f6219880
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-04-26 19:52:26 -0300

    refactor: change created_at and updated_at as date and add json

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
+import (
+	"time"
+)
+
 type Book struct {
-	id string
-	title string
-	description string
-	author string
-	genre string
-	publication_date string
-	created_at string
-	updated_at string
+	id string `json:"id"`
+	title string `json:"title"`
+	description string `json:"description"`
+	author string 	`json:"author"`
+	genre string 	`json:"genre"`
+	publication_date string `json:"publication_date"`
+	created_at time.Time `json:"created_at"`
+	updated_at time.Time `json:"updated_at"`
+}
+
+func main() {
+	println("Hello, World!")
 }
